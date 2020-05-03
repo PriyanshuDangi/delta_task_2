df4bd2e5799890212374a5dd2c3b7c81a3a62e10
PriyanshuDangi <priyanshudangipd@gmail.com>
2020-05-03 23:15:44 +0530

    added pause and resume

--- chris.js
+++ chris.js
@@ -16,6 +16,7 @@ var myGameArea = {
     stop: function(){
         clearInterval(this.interval);
         canvasElement.removeEventListener('click', jumpClickFunction);
+        canvasElement.removeEventListener('click', clickImage);
         // backgroundMusic.stop();
         bestScoreFunction();
         deadSound.play();
@@ -44,7 +45,7 @@ var myGameArea = {
             ctx.fillText("Highest Score: " + bestScore, centerWidth, centerHeight +10); 
             ctx.font = "15px Consolas";
             setTimeout(() => {
-                ctx.fillText("click here to play again", centerWidth, centerHeight + 40);
+                ctx.fillText("click anywhere to play again", centerWidth, centerHeight + 40);
                 canvasElement.addEventListener('click', screen);
             },700)
         }, 100);
@@ -53,7 +54,57 @@ var myGameArea = {
     }
 }
 
+// var gamestate = 1;
 var score = 0;
+const mouse = {
+    x: myGameArea.canvas.width,
+    y: myGameArea.canvas.height
+}
+var toggle;
+// var soundtoggle;
+// addEventListener('click', clickImage);
+
+function clickImage(e){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    toggle = pauseandplay.check();
+    pauseandplay.update();
+    if(toggle){
+        canvasElement.removeEventListener('click', jumpClickFunction);
+        console.log(toggle);
+        console.log(pauseandplay.state);
+        if(pauseandplay.state == 0){ //pause
+            clearInterval(myGameArea.interval);
+            // canvasElement.removeEventListener('click', jumpClickFunction);
+        }else{ //play
+            setTimeout(function(){
+                canvasElement.addEventListener('click', jumpClickFunction);
+                myGameArea.intervalFunction();
+            }, 300);
+            // canvasElement.addEventListener('click', jumpClickFunction);
+        }
+    }
+
+    // soundtoggle = soundOnAndOff.check();
+    // soundOnAndOff.update();
+    // if(soundtoggle){
+    //     if(soundOnAndOff.state == 0){ //pause
+    //         soundVolume(0);
+    //     }else{ //play
+    //         soundVolume(1);
+    //     }
+    // }
+}
+
+// addEventListener('mousedown', function(e){
+//     mouse.x = event.x;
+//     mouse.y = event.y;
+// })
+
+// addEventListener('mouseup', function(e){
+//     mouse.x = myGameArea.canvas.width;
+//     mouse.y = myGameArea.canvas.height
+// })
 
 function component(x, y){
     this.x = x;
@@ -203,7 +254,7 @@ function obstacle(x, y, color, angle1, angle2){
     this.firstTime = 0;
     this.update = function(){
         this.radians += this.velocity;
-        if(this.y - this.yOnTap > 25){
+        if(this.y - this.yOnTap > 35){
             this.dy = 0;
         }
         this.y += this.dy;
@@ -265,6 +316,47 @@ function rectangle(x, y, color, width, height, type){
     }
 }
 
+function imageButton(src1, src2, x, y, width, height){
+    // this.src = src1;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.state = 1;
+
+    this.image = new Image();
+    // this.image.src = src;.
+    // console.log(this.src);
+    this.draw = function(){
+        var ctx = myGameArea.context;
+        // ctx.drawImage(this.image.src, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.src, this.x, this.y, this.width, this.height);
+    }
+    this.update = function(){
+        // if(this.state == 1){
+        //     if(mouse.x - (this.x + this.width/2) <= this.width && mouse.x - (this.x + this.width/2) >= -this.width && mouse.y - (this.y + this.height/2) <= this.height && mouse.y - (this.y + this.height/2) >= -this.height){
+        //         console.log("hello");
+        //         this.state = 0;
+        //     }
+        // }
+        if(this.state == 1){
+            this.src = src1;
+        }else{
+            this.src = src2;
+        }
+        this.draw();
+    }
+    this.check = function(){
+        // if(this.state == 1){
+            if(mouse.x - (this.x + this.width/2) <= this.width/2 && mouse.x - (this.x + this.width/2) >= -this.width/2 && mouse.y - (this.y + this.height/2) <= this.height/2 && mouse.y - (this.y + this.height/2) >= -this.height/2){
+                // console.log("hello");
+                this.state = this.state == 1 ? 0 : 1;
+                return true;
+            }
+            return false;
+        // }
+    }
+}
 
 var canvasElement;
 function startScreen(){
@@ -314,9 +406,13 @@ var jumpSound;
 var backgroundMusic;
 var deadSound;
 var myScore;
+var pauseandplay;
+var soundOnAndOff;
 function init(){
+    // gamestate = 1;
     canvasElement.removeEventListener('click', screen);
     canvasElement.addEventListener('click', jumpClickFunction);
+    canvasElement.addEventListener('click', clickImage);
     // myGameArea.start();
     // myGameArea.intervalFunction();
     myObstacle = [];
@@ -331,6 +427,19 @@ function init(){
     myScore = new rectangle(myGameArea.canvas.width/40, myGameArea.canvas.height/15, "white", "40px", "Consolas", "score");
     jumpSound = new sound("./assets/sounds/jump.wav");
     deadSound = new sound("./assets/sounds/dead.wav");
+    var img1 = document.getElementById('pause');
+    var img2 = document.getElementById('play');
+    pauseandplay = new imageButton(img1, img2, myGameArea.canvas.width - myGameArea.canvas.width/15, myGameArea.canvas.height/30, myGameArea.canvas.width/20, myGameArea.canvas.width/20);
+    console.log(pauseandplay);
+    // var img3 = document.getElementById('soundon');
+    // var img4 = document.getElementById('soundof');
+    // soundOnAndOff = new imageButton(img3, img4, myGameArea.canvas.width - 2*myGameArea.canvas.width/15, myGameArea.canvas.height/30, myGameArea.canvas.width/20, myGameArea.canvas.width/20);
+    // // soundVolume(soundtoggle);
+    // if(!soundtoggle){
+    //     console.log(soundtoggle);
+    //     soundVolume(0);
+    // }
+    
 }
 // init();
 
@@ -369,6 +478,8 @@ function updateGameArea(){
     // myObstacle.update();
     // myObstacle1.update();
     myScore.draw();
+    // soundOnAndOff.update();
+    pauseandplay.update();
     gamePiece.update();
 }
 
@@ -383,7 +494,7 @@ var gamePieceYOnTap;
 // addEventListener('click', jumpClickFunction);
 
 function jumpClickFunction(){
-    // console.log('hi');
+    console.log('hi');
     jumpSound.play();
     gamePieceYOnTap = gamePiece.y;
     gamePiece.dy = -2;
@@ -391,7 +502,14 @@ function jumpClickFunction(){
         obstacle.yOnTap = obstacle.y;
         obstacle.dy = 3;
     })
+
     // myObstacle.y +=7;
     // myObstacle1.y +=7;
     // gamePiece.gravity = -0.05;
 }
+
+// function soundVolume(volume){
+//     backgroundMusic.sound.volume = volume;
+//     jumpSound.sound.volume = volume;
+//     deadSound.sound.volume = volume;
+// }
